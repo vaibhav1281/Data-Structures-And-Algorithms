e51018b7ce15e637024f72bc4263b0da5e0d2d4f
vaibhav1281 <vaibhav1281saroj@gmail.com>
2024-01-07 17:31:37 +0530

    Patterns

---
+++ Patterns/pattern1.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>       
+using namespace std;
+
+void pattern1(int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << "* ";
+        }
+       
+        cout << endl;
+    }
+}
+
+int main()
+{   
+    int N = 4;
+
+    pattern1(N);
+
+    return 0;
+}
+
+/* OutPut
+
+    * * * * 
+    * * * * 
+    * * * * 
+    * * * * 
+*/
